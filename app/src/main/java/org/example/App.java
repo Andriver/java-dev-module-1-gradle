b2a8e0d3343f33f8d488d611d8dfecd7da391d33
APrudnik <aprudnik@gmail.com>
2023-07-10 16:18:26 +0300

    gradle

---
+++ app/src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class App {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    public static void main( String[] args )
+    {
+
+        Map<String, String> myName = new TreeMap<>(Comparator.reverseOrder());
+        myName.put("lastName", "Prudnyk");
+        myName.put("name", "Andriy");
+        System.out.println(GSON.toJson(myName));
+
+    }
+}
